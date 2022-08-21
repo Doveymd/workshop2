49ed5e86f0a6925000a562e17e0e63470f602eb9
Dovey LaPee <dovey_lapee@yahoo.com>
2022-08-21 14:06:32 -0500

    commit new infot

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 301566501613.dkr.ecr.us-east-2.amazonaws.com, '0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ec2-3-135-91-52.us-east-2.compute.amazonaws.com',
+                 '0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
